8d1672f6331f5f8c3074c1bcf6b9202568e41e9a
Alex Keating <keating.dev@protonmail.com>
2021-09-21 08:30:12 -0400

    Compiling contract

---
+++ contracts/PaymentBridgeFactory.sol
@@ -0,0 +1,70 @@
+/* SPDX-License-Identifier: MIT */
+pragma solidity 0.8.6;
+
+import "@openzeppelin/contracts-upgradeable/proxy/ClonesUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
+import "./PaymentBridge.sol";
+
+contract PaymentBridgeFactory is Initializable{
+    using AddressUpgradeable for address;
+    using ClonesUpgradeable for address;
+    // Methods
+    /// @dev fixed contract template for EIP-1167 Proxy pattern
+    address public template;
+
+    // init unchained
+    /// @dev used to handle multiple inheritance in order to prevent
+    /// calling parent intializers
+    function __PaymentBridgeFactory_init_unchained(address _template) internal initializer {
+        template = _template;
+    }
+    // init
+    // @dev embeds linearized calls to all parent initializers
+    function __PaymentBridgeFactory_init(address _template) internal initializer {
+        __PaymentBridgeFactory_init_unchained(_template);
+    }
+    // initialize
+    /// @dev Initializes factory contract using a minimal proxy pattern (EIP-1167)
+    function initialize(address _template) public{
+        __PaymentBridgeFactory_init(_template);
+    }
+
+    
+    // _initAndEmit - will house payment logic
+    function _initAndEmit(address _instance, address sender, bytes calldata _initData) private {
+        if (_initData.length > 0) {
+            _instance.functionCall(_initData);
+        }
+        // PaymentBridge bridge = PaymentBridge(_instance);
+        // require(bridge.initialized(), "PaymentBridgeFactory: is not initialized");
+    }
+
+
+    // clone
+    function clone(bytes calldata _initData) internal {
+        address sender = msg.sender;
+        _initAndEmit(template.clone(), sender, _initData);
+    }
+
+    // create payment bridge
+    function createPaymentBridge(bytes calldata _initData) external {
+        require(template != address(0), "PaymentBridgeFactory: Missing PaymentBridge Template");
+        clone(_initData);
+    }
+    // totalBridges
+    // bridges owned by
+
+    // Event
+    //
+    // New Bridge
+
+    // _gap
+    // This is empty reserved space in storage that is put in place in Upgradeable contracts.
+    // It allows us to freely add new state variables in the future without compromising the
+    // storage compatibility with existing deployments
+    // The size of the __gap array is calculated so that the amount of storage used by a contract
+    // always adds up to the same number
+    uint256[50] private __gap;
+
+}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,5 @@
 import "@nomiclabs/hardhat-waffle";
+import "@openzeppelin/hardhat-upgrades";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
