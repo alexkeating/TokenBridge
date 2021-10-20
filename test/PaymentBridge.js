9227adfefd4fca28b6a2f0ebbc54a95ac01e0516
Keating <keating.dev@protonmail.com>
2021-10-20 15:32:24 -0400

    WIP tests

---
+++ test/PaymentBridge.js
@@ -0,0 +1,20 @@
+// Test cases
+//
+// 1. Make sure payee, feeAmount, template are set
+// 2. Make sure fee can be updated
+// 3. Make sure Pay goes to the bridge
+// 4. Make sure poke releases stuck funds
+// 5. Make sure receive sends to correct bridge
+
+
+describe("PaymentBridge", () => {
+    let dai;
+    let weth;
+    let omnibridge;
+    let xdaibridge;
+    let paymentBridgeFactory;
+    
+    before(async () => {
+        // sruff    
+    })
+})
